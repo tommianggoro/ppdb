b90f52a235d69b79f87516478729cb08766c42f8
Tommy Anggoro Yunar <tommianggoro@outlook.com>
2023-02-01 20:16:33 +0700

    Add information on dashboard

--- application/views/backend/dashboard/index.php
+++ application/views/backend/dashboard/index.php
@@ -16,5 +16,34 @@
             <!-- /.col -->
         </div>
         <!-- /.row -->
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title">Status</h5>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-6">
+                    <?php $roleId = $this->session->userdata('role_id'); ?>
+                    <?php if($roleId == ROLE_CANDIDATE): ?>
+                      <h2>Mohon untuk dapat menunggu tahap seleksi administrasi :) </h2>
+                    <?php elseif($roleId == ROLE_PASS): ?>
+                      <h2>Selamat anda telah lulus :) </h2>
+                    <?php elseif($roleId == ROLE_NOT_PASS): ?>
+                      <h2>Mohon maaf anda tidak lulus :( </h2>
+                    <?php endif; ?>
+                  </div>
+                </div>
+                <!-- /.row -->
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
     </div><!--/. container-fluid -->
 </section>
